654c3558066af785b4c106c8148cf41edd5e804c
Hypo Soc <hyposoc1@email.com>
2026-04-06 23:24:48 -0400

    Fixing card pool

--- MnemonistCode/Cards/Special/Amnesia.cs
+++ MnemonistCode/Cards/Special/Amnesia.cs
@@ -1,16 +1,35 @@
+using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using Mnemonist.MnemonistCode.Extensions;
 using Mnemonist.MnemonistCode.Relics;
 
 namespace Mnemonist.MnemonistCode.Cards.Special;
 
-public class Amnesia() : MnemonistCard(0,
+[Pool(typeof(TokenCardPool))]
+public class Amnesia() : CustomCardModel(0,
     CardType.Skill, CardRarity.Curse,
     TargetType.Self)
 {
+    //Image size:
+    //Normal art: 1000x760 (Using 500x380 should also work, it will simply be scaled.)
+    //Full art: 606x852
+    public override string CustomPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+
+    //Smaller variants of card images for efficiency:
+    //Smaller variant of fullart: 250x350
+    //Smaller variant of normalart: 250x190
+
+    //Uses card_portraits/card_name.png as image path. These should be smaller images.
+    public override string PortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+    public override string BetaPortraitPath => $"beta/{Id.Entry.ToLowerInvariant()}.png".CardImagePath();
+
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Anamnesis>()];
     public override HashSet<CardKeyword> CanonicalKeywords =>
     [
